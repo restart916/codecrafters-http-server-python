5af89329fe827b83c679e84456ef307f9ea0f1f7
Yongsang Lee <restart916@gmail.com>
2024-07-06 19:28:06 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,28 @@
 import socket
 
 
+def is_exist(request_target):
+    print('target', request_target)
+    return request_target == b"/"
+
 def main():
-    # The tester will then send an HTTP GET request to your server:
-
-    # $ curl -v http://localhost:4221
-    # Your server must respond to the request with the following response:
-
-    # HTTP/1.1 200 OK\r\n\r\n
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
 
     while True:
         client_socket, addr = server_socket.accept()
         data = client_socket.recv(1024)
-        print(data)
-        client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        # print(data)
+
+        request_line = data.split(b"\r\n")[0]
+        headers = data.split(b"\r\n")[1:]
+
+        request_target = request_line.split(b" ")[1]
+        if is_exist(request_target) == False:
+            client_socket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        else:
+            client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
         client_socket.close()
-        
+
 
 
 if __name__ == "__main__":
